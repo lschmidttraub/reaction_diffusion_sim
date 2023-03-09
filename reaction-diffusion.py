219d5a39cc2062d0a6d04ac654eef8aaa0b32556
spicyboi26 <leo.schmidttraub@gmail.com>
2023-03-09 20:27:43 +0100

    documentation rudimentaire, changement du nom des variables en français

--- reaction-diffusion.py
+++ reaction-diffusion.py
@@ -4,14 +4,20 @@ import matplotlib.animation as animation
 import seaborn as sns
 import cv2 as cv
 
-def laplacian1D(a, dx):
+def laplacien1D(a, dx):
+    '''
+    Calcul de l'opérateur laplacien discrétisé dans un tableau à une dimension
+    '''
     return (
         - 2 * a
         + np.roll(a,1,axis=0) 
         + np.roll(a,-1,axis=0)
     ) / (dx ** 2)
 
-def laplacian2D(a, dx):
+def laplacien2D(a, dx):
+    '''
+    Calcul de l'opérateur laplacien discrétisé dans un tableau à 2 dimensions
+    '''
     return (
         - 4 * a
         + np.roll(a,1,axis=0) 
@@ -20,185 +26,219 @@ def laplacian2D(a, dx):
         + np.roll(a,-1,axis=1)
     ) / (dx ** 2)
 
-def random_initialization(shape):
+def initialisation_aleatoire(shape):
+    '''
+    Permet de générer deux tableau à valeurs aléatoires a et b
+    '''
     return (np.random.normal(loc=0, scale=0.05, size=shape), np.random.normal(loc=0, scale=0.05, size=shape))
 
 class Simulation1D():
-    def __init__(self, Da, Db, Ra, Rb, initializer, width=1000, dx=1, dt=0.1, steps=1):
+    '''
+    Simulation d'un système de réaction-diffusion dans une dimension
+    '''
+    def __init__(self, Da, Db, Ra, Rb, f_init, largeur=1000, dx=1, dt=0.1, etapes=1):
         self.Da = Da
         self.Db = Db
         self.Ra = Ra
         self.Rb = Rb
-        self.initializer = initializer
-        self.width = width
+        self.f_init = f_init
+        self.largeur = largeur
         self.dx = dx
         self.dt = dt
-        self.steps = steps
+        self.etates = etapes
     
-    def initialize(self):
+    def initialisation(self):
+        '''
+        Initialise les paramètres de la simulation
+        '''
         self.t = 0
-        self.a, self.b = self.initializer(self.width)
+        self.a, self.b = self.f_init(self.largeur)
     
-    def update(self):
-        for i in range(self.steps):
+    def actualiser(self):
+        '''
+        Actualise la fonction pour simuler le système de réaction diffusion
+        '''
+        for _ in range(self.etapes):
             self.t += self.dt
-            lapA = laplacian1D(self.a, self.dx)
-            lapB = laplacian1D(self.b, self.dx)
+            lapA = laplacien1D(self.a, self.dx)
+            lapB = laplacien1D(self.b, self.dx)
             deltaA = self.dt*(self.Da*lapA+self.Ra(self.a,self.b))
             deltaB = self.dt*(self.Db*lapB+self.Rb(self.a,self.b))
             self.a += deltaA
             self.b += deltaB
 
 
-    def draw(self, ax):
+    def tracer(self, ax):
+        '''
+        Trace la figure
+        '''
         ax.clear()
         ax.plot(self.a, color="b", label="A")
         ax.plot(self.b, color="g", label="B")
         ax.legend()
         ax.set_ylim(-1,1)
-        ax.set_title("t = {:.2f}".format(self.t))
+        ax.set_title("t = "+str(self.t))
     
-    def initialize_figure(self):
+    def initialisation_graphique(self):
+        '''
+        Initialise le graphique matplotlib
+        '''
         fig, ax = plt.subplots()
         return fig, ax
     
-    def plot_evolution_outcome(self, filename, n_steps):
-        """
-        Evolves and save the outcome of evolving the system for n_steps
-        """
-        self.initialize()
-        fig, ax = self.initialize_figure()
+    def creer_image(self, fichier, nb_etapes):
+        ''''
+        Sauvegarde le résultat après nb_etapes itérations dans un fichier image
+        '''  
+        self.initialisation()
+        fig, ax = self.initialisation_graphique()
         
-        for _ in range(n_steps):
-            self.update()
+        for _ in range(nb_etapes):
+            self.actualiser()
 
-        self.draw(ax)
-        fig.savefig(filename)
+        self.tracer(ax)
+        fig.savefig(fichier)
         plt.close()
 
-    def plot_time_evolution(self, filename, n_steps=30):
-        """
-        Creates a gif from the time evolution of a basic state syste.
-        """
-        self.initialize()
-        fig, ax = self.initialize_figure()
+    def creer_gif(self, fichier, nb_etapes=30):
+        '''
+        Génère un GIF de l'évolution de la courbe à travers le temps
+        '''
+        self.initialisation()
+        fig, ax = self.initialisation_graphique()
 
-        def step(t):
-            self.update()
-            self.draw(ax)
+        def etape(t):
+            self.actualiser()
+            self.tracer(ax)
 
-        anim = animation.FuncAnimation(fig, step, frames=np.arange(n_steps), interval=20)
-        anim.save(filename=filename, dpi=60, fps=10, writer='pillow')
+        anim = animation.FuncAnimation(fig, etape, frames=np.arange(nb_etapes), interval=20)
+        anim.save(filename=fichier, dpi=60, fps=10, writer='pillow')
         plt.close()
 
 class Simulation2D():
-    def __init__(self, Da, Db, Ra, Rb, initializer, width=1000, height = 1000, dx=1, dt=0.1, steps=1):
+    '''
+    Simulation d'un système de réaction-diffusion dans 2 dimensions
+    '''
+    def __init__(self, Da, Db, Ra, Rb, f_init, largeur=1000, hauteur = 1000, dx=1, dt=0.1, etapes=1):
         self.Da = Da
         self.Db = Db
         self.Ra = Ra
         self.Rb = Rb
-        self.initializer = initializer
-        self.width = width
-        self.height = height
+        self.f_init = f_init
+        self.largeur = largeur
+        self.hauteur = hauteur
         self.dx = dx
         self.dt = dt
-        self.steps = steps
+        self.etapes = etapes
     
-    def initialize(self):
+    def initialisation(self):
+        '''
+        Initialise les paramètres de la simulation
+        '''
         self.t = 0
-        self.a, self.b = self.initializer((self.height, self.width))
+        self.a, self.b = self.f_init((self.hauteur, self.largeur))
     
-    def update(self):
-        for i in range(self.steps):
+    def actualiser(self):
+        '''
+        Actualise la fonction pour simuler le système de réaction diffusion
+        '''
+        for _ in range(self.etapes):
             self.t += self.dt
-            self._update()
-            
-    def _update(self):
-        lapA = laplacian2D(self.a, self.dx)
-        lapB = laplacian2D(self.b, self.dx)
-        deltaA = self.dt*(self.Da*lapA+self.Ra(self.a,self.b))
-        deltaB = self.dt*(self.Db*lapB+self.Rb(self.a,self.b))
-        self.a += deltaA
-        self.b += deltaB
+            lapA = laplacien2D(self.a, self.dx)
+            lapB = laplacien2D(self.b, self.dx)
+            deltaA = self.dt*(self.Da*lapA+self.Ra(self.a,self.b))
+            deltaB = self.dt*(self.Db*lapB+self.Rb(self.a,self.b))
+            self.a += deltaA
+            self.b += deltaB
 
-    def draw(self, ax):
+    def tracer(self, ax):
+        '''
+        Trace le graphique du tableau de concentrations de la substance A
+        '''
         ax.clear()
-
         ax.imshow(self.a)
-
-        ax.set_title("A, t = {:.2f}".format(self.t))
+        ax.set_title("A, t = "+str(self.t))
     
-    def initialize_figure(self):
+    def initialisation_graphique(self):
+        '''
+        Initialise le graphique matplotlib
+        '''
         fig, ax = plt.subplots(figsize = (10,10))
         return fig, ax
     
-    def real_time_sim(self):
-        self.initialize()
+    def simulation(self, couleur=False):
+        self.initialisation()
         cv.namedWindow('simulation', cv.WINDOW_KEEPRATIO)
         cv.resizeWindow('simulation', 300, 300)
         while True:
-            self.update()
-            cv.imshow("simulation", self.a*255)
-            if cv.waitKey(10) & 0xFF == ord('q'):
+            self.actualiser()
+            if couleur:
+                image = np.stack((self.a*255, np.zeros((self.largeur, self.hauteur)), self.b*255), axis=2)
+            else:
+                image = self.a*255
+            cv.imshow("simulation", image)
+            if cv.waitKey(10) == ord('q'):
                 break
         cv.destroyAllWindows()
 
-    def create_png(self, filename, n_steps):
-        """
-        Evolves and save the outcome of evolving the system for n_steps
-        """
-        self.initialize()
-        fig, ax = self.initialize_figure()
-        for i in range(n_steps):
-            self.update()
+    def creer_image(self, fichier, nb_etapes):
+        ''''
+        Sauvegarde le résultat après nb_etapes itérations dans un fichier image
+        '''  
+        self.initialisation()
+        fig, ax = self.initialisation_graphique()
+        for i in range(nb_etapes):
+            self.actualiser()
         
         
-        self.draw(ax)
-        fig.savefig(filename)
+        self.tracer(ax)
+        fig.savefig(fichier)
         plt.close()
 
-    def create_gif(self, filename, n_steps=30):
-        """
-        Creates a gif from the time evolution of a basic state syste.
-        """
-        self.initialize()
-        fig, ax = self.initialize_figure()
-        def step(t):
-            self.update()
-            self.draw(ax)
+    def creer_gif(self, fichier, nb_etapes=30):
+        '''
+        Génère un GIF de l'évolution de la concentration de la substance A à travers le temps
+        '''
+        self.initialisation()
+        fig, ax = self.initialisation_graphique()
+        def etape(t):
+            self.actualiser()
+            self.tracer(ax)
 
-        anim = animation.FuncAnimation(fig, step, frames=np.arange(n_steps), interval=20)
-        anim.save(filename=filename, dpi=60, fps=10, writer='pillow')
+        anim = animation.FuncAnimation(fig, etape, frames=np.arange(nb_etapes), interval=20)
+        anim.save(filename=fichier, dpi=60, fps=10, writer='pillow')
         plt.close()
     
-    def create_video(self, filename, n_steps=30):
-        """
-        Creates a video from the time evolution of a basic state syste.
-        """
-        self.initialize()
-        video = cv.VideoWriter(filename, cv.VideoWriter_fourcc(*"FFV1"), fps=10, frameSize=(100, 100), isColor=False)
-        for i in range(n_steps):
-            self.update()
-            frame = self.a*255
-            video.write(frame)
-            # if i%10==0:
-            #     cv.imshow("simulation", frame)
-            #     cv.waitKey(0)
-            #     cv.destroyAllWindows()
+    def creer_video(self, fichier, nb_etapes=30, couleur=False):
+        '''
+        Créé une vidéo de la réaction, en couleur ou en noir et blanc
+        '''
+        self.initialisation()
+        if couleur:
+            video = cv.VideoWriter(fichier, cv.VideoWriter_fourcc(*"mp4v"), fps=10.0, frameSize=(self.largeur, self.hauteur))
+        else:
+            video = cv.VideoWriter(fichier, cv.VideoWriter_fourcc(*"mp4v"), fps=10.0, frameSize=(self.largeur, self.hauteur), isColor=False)
+
+        for _ in range(nb_etapes):
+            self.actualiser()
+            if couleur:
+                image = np.stack((self.a*255, np.zeros((self.largeur, self.hauteur)), self.b*255), axis=2)
+            else:
+                image = self.a*255
+            video.write(image.astype(np.uint8))
         video.release()
 
-Da, Db, alpha, beta = 1, 100, -0.005, 10
+Da, Db, alpha, beta = 1, 40, -0.005, 15
 
 def Ra(a,b): 
     return a - a ** 3 - b + alpha
 def Rb(a,b): 
     return (a - b) * beta
 
-width = 100
+largeur = 250
 dx = 1
 dt = 0.001
-Sim2  = Simulation2D(Da, Db, Ra, Rb, random_initialization, width = width, height = width, dx=dx, dt=dt, steps=250)
-# Sim2.plot_time_evolution("graph.avi", 50)
-# Sim2.plot_evolution_outcome("output.png", 50)
-# Sim2.real_time_sim()
-
+Sim2  = Simulation2D(Da, Db, Ra, Rb, initialisation_aleatoire, largeur = largeur, hauteur = largeur, dx=dx, dt=dt, etapes=250)
+Sim2.creer_video("rendu.mp4", 250, couleur=True)
+# Sim2.creer_image("output.png", 50)
+# Sim2.simulation(couleur=True)
